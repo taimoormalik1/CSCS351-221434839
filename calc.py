45c9a9daf5cbde0fe442effc3e5ce958c3226d66
taimoormalik1 <110344847+taimoormalik1@users.noreply.github.com>
2022-07-31 20:44:46 +0500

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return (a + b)
+
+def subtract(a,b):
+    return (a-b)
+
+def square(a,b):
+    return (a ** b)
+
+def mod(a,b):
+    return (a%b)
+
+def multiply(a,b):
+    return (a * b)
